fd596673d92595d1af79d32943ef876e94ea910f
PaomFarv <omor.faruk.myself@gmail.com>
2025-03-23 22:53:02 +0600

    Added voice overs

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,6 @@
 import random
 from colorama import Fore
+import os
 
 colors = ["R","G","B","Y","O","P","C"]
 
@@ -13,14 +14,35 @@ score = 0
 print()
 print(Fore.LIGHTRED_EX + "^"*170)
 print(Fore.LIGHTGREEN_EX + "Welcome to Mastermind Colour Match.")
+os.system('powershell -Command "Add-Type –AssemblyName System.Speech; '
+          '$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+          '$speak.Speak(\'Welcome to Mastermind Colour Match.\');"')
+
 input("Press ENTER to continue...")
 print(Fore.LIGHTBLUE_EX +"\nYou have to guess 4 colours using their first letters separated by spaces.")
+os.system('powershell -Command "Add-Type –AssemblyName System.Speech; '
+          '$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+          '$speak.Speak(\'You have to guess 4 colours using their first letters separated by spaces.\');"')
+
 input("Press ENTER to continue...")
 print("\nHere are all the colours used in this game: 'R' (Red),'G' (Green),'B' (Blue),'Y' (Yellow),'O' (Orange),'P' (Purple),'C' (Cyan).")
+os.system('powershell -Command "Add-Type –AssemblyName System.Speech; '
+          '$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+          '$speak.Speak(\'Here are all the colours used in this game:\');"')
+
 input("Press ENTER to continue...")
 print("\nYou have 10 attempts to guess the correct colours in the correct order.")
+os.system('powershell -Command "Add-Type –AssemblyName System.Speech; '
+          '$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+          '$speak.Speak(\'You have 10 attempts to guess the correct colours in the correct order.\');"')
+
 input("Press ENTER to continue...")
-print("\nSymbols used to represent -\n'✅' denotes correct position, '⚠️  ' correct guess but wrong position, '❌' wrong guess.")
+print("\nSymbols used to represent -\n'✅ ' denotes correct position, '⚠️ ' correct guess but wrong position, '❌ ' wrong guess.")
+os.system('powershell -Command "Add-Type –AssemblyName System.Speech; '
+          '$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+          '$speak.Speak(\'Symbols used to represent -\');"')
+
+
 print(Fore.LIGHTRED_EX + "^"*170)
 
 attempts = 10
